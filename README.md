913cd31d25fb04f0e1668411ba46fca957273452
Sophia Kiani <sophkiani@gmail.com>
2021-10-12 06:36:52 -0500

    updated the overall styling

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Sophk_Weather_Gadget
+# PinkSO Weather Gadget
 A simple fun weather dashboard created with html, css, and javascript.
+
+## Link To Deployed Page
+    
+
+
+## Tech
+
+    - HTML
+    - CSS
+    - Bootstrap
+    - Javascript
+    - jQuery
+    - OpenWeather API
+
+
+## Preview
+
+
+
+## Link to Github Repo
+
+
+
+## Bugs 
+
+    Currently there are no known bugs.
---
+++ assests/js/main.js
@@ -0,0 +1,37 @@
+///////////////////////////////// Selectors ////////////////////////////
+// API Variables
+const apiKey = "00ef83945ac64f890995ea9f7422b1b0";
+const locationName = "";
+
+
+
+
+// API
+let todayWeather = {
+
+}
+
+
+let sevenDayWeather = {
+  'apiKey': "00ef83945ac64f890995ea9f7422b1b0",
+  fetchWeather:  function () {
+    fetch ("api.openweathermap.org/data/2.5/weather?q=" 
+      + locationName 
+      + "&appid=" 
+      + this.apiKey
+      + "&cnt=7"
+      + "&units=imperial"
+    )
+    .then((response) => response.json())
+    };
+  displayWeather: function(data) {  
+    const cityName = data;
+    const icon =
+    const descriptio = data.weather
+    const speed = data.wind;
+  }
+
+};
+
+
+
