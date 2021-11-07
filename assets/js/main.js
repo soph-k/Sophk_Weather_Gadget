774fdcde8b17da7003875113e7103e414c756872
Sophia <sophkiani@gmail.com>
2021-11-06 22:36:50 -0500

    updated styling and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,6 @@ const searchedDescription = $('.description')
 const searchedIcon = $('.current_weather_data')
 const searchedWeather = $('.today_temp')
 
-
-
-
 const humidityEl = $('.humidity');
 const humidityValueEl = $('.humidity_value');
 const pressureEl = $('.pressure');
@@ -19,112 +16,114 @@ const windEl = $('.wind');
 const windValueEl = $('.wind_value');
 const uvEl = $('.uv');
 const uvValueEl = $('.uv_value');
+const lat =$('.lat')
+const lon =$('.lon')
+
+
 
 const weeklyDayEl = $('.weeks_forecast');
 
 const searchBtn = $('.search_btn');
-const searchbarEl = $('.search_bar')
+const searchbarEl = $('.search_input')
 // API Variables
 const apiKey = "00ef83945ac64f890995ea9f7422b1b0";
 const locationName = searchbarEl.val();
 
 
-
 ///////////////////////////////// API ////////////////////////////
-function weatherData () {
-  $.ajax({
-    url: `https://api.openweathermap.org/data/2.5/forecast?q=${locationName}&units=imperial&appid=${apiKey}`,
-    method: 'GET'
-  }).then((response) => response.json()).then(response2 => {
-    console.log(response2)
-//within this promise because of response2 what we are going to do is listen for any event on related to the search bar 
-// determine what type of event was fired using if/else statements
-// call weatherdata function to display weather data
-    searchbarEl.change(function(event) {
-    event.preventDefault();
-      if (event.keyCode === 13) {
-        currentWeatherData(response2);
-      }
-      else {
-      searchBtn.click(currentWeatherData(response2));
+searchbarEl.change((event, locationName) => {
+  event.preventDefault();
+    if (event.keyCode === 13 || searchBtn.click()) {
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${locationName}&appid=${apiKey}&units=imperial`,
+        method: 'GET'
+      }).then((response) => response.json()).then(data => {
+        console.log(data)
+        const city = data.name;
+        const country = data.city.country;
+        const description = data.city.weather[0].description;
+        const icon = "https://openweathermap.org/img/wn/" + day.weather[0].icon + ".png";
+        const temp = data.temperature.value;
+        const tempUnit = data.temperature.unit;
+        const humidity = data.humidity.value;
+        const humidityUnit = data.humidity.unit
+        const pressure = data.pressure.value;
+        const pressureUnit = data.pressure.unit;
+        const wind = data.wind.speed.value;
+        const windUnit = data.wind.speed.unit;
+        const uv = data.uv.
+
+        searchedLocation.html(`${city}, ${country}`);
+        searchedlat.html(`${lat}`);
+        searchedlon.html(`${lon}`);
+        searchedTime.html(`${time}`);
+        searchedDescription.html(`${description}`);
+        searchedIcon.html(`${icon}`);
+        searchedWeather.html(`${temp} ${tempUnit}`);
+
+        if (uv < 3) {
+          uvValueEl.addClass() 
+        } else if (uv < 6) {
+          uvValueEl.addClass()  
+        } else if (uv < 8) {
+          uvValueEl.addClass()  
+        } else if (uv < 11) {
+          uvValueEl.addClass()  
+        } else {
+          uvValueEl.addClass()
+        }
+      })
+    .catch((err) => alert('City does not exist'));
     }
-  })
-  
-  // .catch((err) => alert('City does not exist'));
-  })
-}
-
-function currentWeatherData(data) {
-  const city = data.city.name;
-  const country = data.city.country;
-  const lat = data.city.coord.lat;
-  const lon = data.city.coord.lon;
-  const time = data.timezone;
-  const description = data.city.weather[0].description;
-  const icon = data.weather[0].icon;
-  const temp = data.temperature.value;
-  const tempUnit = data.temperature.unit;
-  const humidity = data.humidity.value;
-  const humidityUnit = data.humidity.unit
-  const pressure = data.pressure.value;
-  const pressureUnit = data.pressure.unit;
-  const wind = data.wind.speed.value;
-  const windUnit = data.wind.speed.unit;
-  const uv = data.uv
-
-// https://openweathermap.org/current
-
-  searchedLocation.text(`${city}, ${country}`);
-  searchedlat.text(`${lat}`);
-  searchedlon.text(`${lon}`);
-  searchedTime.text(`${time}`);
-  searchedDescription.text(`${description}`);
-  searchedIcon.text(`${icon}`);
-  searchedWeather.text(`${temp} ${tempUnit}`);
-
-  uvColor(data);
-
-  
-}
-
-function uvColor(data) {
-  if (uv < 3) {
-    uvValueEl.addClass() 
-  } else if (uv < 6) {
-    uvValueEl.addClass()  
-  } else if (uv < 8) {
-    uvValueEl.addClass()  
-  } else if (uv < 11) {
-    uvValueEl.addClass()  
-  } else {
-    uvValueEl.addClass()
-  }
-}
-
-function weeklyWeatherData(data) {
-  weeklyDayEl.each(day => day
-
-  )
-}
-///////////////////////////////// Local Storage ////////////////////////////
-
-
-
-
-///////////////////////////////// Event Listeners ////////////////////////////
-searchBtn.click(function(event){
-  if(searchbarEl.val() !== "") {
-    weatherData();
-  }
+  // locationValues(locationName, lat, lon);
 })
 
 
-// searchbarEl.keyup(function(event) {
-//   if (event.keyCode === 13) {
-//   weatherData();
-//    event.preventDefault();
-//    searchBtn.click(weatherData);
-//   } 
-// })
+// function locationValues(locationName, lat, lon) {
+//   $.ajax({
+//     url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial",
+//     method: 'GET'
+//   }).then((response) => response.json()).then(data => {
+//     const lat = data.city.coord.lat;
+//     const lon = data.city.coord.lon;
+
+    
+//     })
 
 
+    
+ 
+
+  
+
+
+
+// function weeklyWeatherData(data) {
+//   weeklyDayEl.each(day => day
+
+//   )
+// }
+// ///////////////////////////////// Local Storage ////////////////////////////
+
+// function saveHighScore () {
+//   const highScore = {
+//     hsname: inputInitalEl.val(),
+//     hsscore: score
+//   }
+//   oldScores.push(highScore);
+//   oldScores.sort(function (a, b) {
+//     return b.hsscore - a.hsscore
+//   })
+//   oldScores.splice(5);
+//   localStorage.setItem("highScoresDB", JSON.stringify(oldScores));
+//   highscores();
+// }
+
+// // Takes scores from local stoarage and displays it in highscores list
+// function highscores() {
+//   displayHighScore.html(oldScores
+//     .map(highScoreArray => {
+//       return `<li>${highScoreArray.hsname}: ${highScoreArray.hsscore}</li>`;     
+//     })
+//   .join(""))
+// }
